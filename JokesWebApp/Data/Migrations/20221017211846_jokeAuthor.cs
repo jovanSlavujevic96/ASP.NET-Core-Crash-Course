ef8ab47a9d0d13324e67bd057d1080876efed1d5
Jovan Slavujevic <jotzilla96@gmail.com>
2022-10-17 23:50:42 +0200

    Added Author column for Joke

---
+++ JokesWebApp/Data/Migrations/20221017211846_jokeAuthor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JokesWebApp.Data.Migrations
+{
+    public partial class jokeAuthor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Joke",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Joke");
+        }
+    }
+}
